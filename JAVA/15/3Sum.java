663af9302bddd435bee92bfa9cedc0124d38a688
MathBenDAI <cfdtlee@gmail.com>
2015-10-23 08:18:38 -0700

    modify 15

--- JAVA/15/3Sum.java
+++ JAVA/15/3Sum.java
@@ -5,7 +5,7 @@ public class Solution {
         for(int k = 0; k < nums.length-2; k++) {
             int i = k + 1, j = nums.length-1; //bound!
             if(k == 0 || (nums[k] != nums[k-1])) { //k has to skip the duplicate nums
-                while(i < j && j <= nums.length-1) { //don't miss '='
+                while(i < j && j < nums.length) { //don't miss '='
                     if(nums[i] + nums[j] + nums[k] == 0) {
                         while(i < j && nums[i] == nums[i+1]) {i++;} //to skip the duplicate nums
                         while(i < j && nums[j] == nums[j-1]) {j--;} //to skip the duplicate nums
