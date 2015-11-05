d79db7a1fafa74d82ce2a973788dcc5c7e18c5c2
MathBenDAI <cfdtlee@gmail.com>
2015-11-05 09:53:21 -0500

    modify

--- JAVA/240/Search_a_2D_Matrix_II.java
+++ JAVA/240/Search_a_2D_Matrix_II.java
@@ -1,16 +1,16 @@
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        return searchMatrix(matrix, target, 0, matrix.length-1, 0, matrix[0].length-1);//
+        return searchMatrix(matrix, target, 0, matrix.length-1, 0, matrix[0].length-1);//i2 and j2 should be matrix.length-1 and ...
     }
     boolean searchMatrix(int[][] matrix, int target, int i1, int i2, int j1, int j2) {
-        System.out.print(i1);System.out.print(i2);System.out.print(j1);System.out.println(j2);
-        if(i1 > i2 || j1 > j2 || i1 < 0 || j1 < 0 || i2 >= matrix.length || j2 >= matrix[0].length) return false; //
+        if(i1 > i2 || j1 > j2 || i1 < 0 || j1 < 0 || i2 >= matrix.length || j2 >= matrix[0].length) return false; //bug easy happen
         int im = (i1 + i2)/2, jm = (j1 + j2)/2;
         int mid = matrix[im][jm];
         if(mid == target) return true;
-        // 左上，右上，左下
-        else if(mid > target) return searchMatrix(matrix, target, i1, im-1, j1, jm-1) || searchMatrix(matrix, target, im+1, i2, j1, jm) || searchMatrix(matrix, target, i1, im, jm+1, j2);
+        // if mid > target, target cannot be in downright
+        // then search upleft, upright, downleft 左上，右上，左下
+        else if(mid > target) return searchMatrix(matrix, target, i1, im, j1, jm-1) || searchMatrix(matrix, target, i1, im-1, jm, j2) || searchMatrix(matrix, target, im+1, i2, j1, jm-1);
         // 右上，左下，右下
-        else return searchMatrix(matrix, target, im+1, i2, j1, jm) || searchMatrix(matrix, target, i1, im, jm+1, j2) || searchMatrix(matrix, target, im+1, i2, jm+1, j2);
+        else return searchMatrix(matrix, target, i1, im, jm+1, j2) || searchMatrix(matrix, target, im+1, i2, j1, jm) || searchMatrix(matrix, target, im+1, i2, jm+1, j2);
     }
 }
